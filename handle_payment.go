e32f32fb9565e3638528c006446e6ba42e408e4e
yihune21 <zewdieyihune@gmail.com>
2025-12-08 23:00:29 +0300

    Add initial payment handling functions with placeholders for Chapa integration

---
+++ handle_payment.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+//TODO:using chapa payment gateway
+
+func initiate(w http.ResponseWriter , r *http.Request)  {
+	//todp
+}
+func refund(w http.ResponseWriter , r *http.Request)  {
+	//todo
+}
+func process(w http.ResponseWriter , r *http.Request)  {
+	//todo
+}
